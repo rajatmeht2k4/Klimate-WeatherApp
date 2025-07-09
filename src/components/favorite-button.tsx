1edc9b4de777e3de714cd29d245709ec80883084
RajatMehta04 <rajatmehta2k4@gmail.com>
2025-07-10 03:38:39 +0530

    Prep for deployement

---
+++ src/components/favorite-button.tsx
@@ -0,0 +1,42 @@
+import type { WeatherData } from "@/api/types"
+import { useFavorite } from "@/hooks/use-favorite";
+import { Button } from "./ui/button";
+import { Star } from "lucide-react";
+import { toast } from "sonner";
+
+interface FavoriteButtonProps {
+    data: WeatherData;
+}
+
+const FavoriteButton = ({ data }: FavoriteButtonProps) => {
+    const { addFavorite, isFavorite, removeFavorite } = useFavorite();
+    const isCurrentlyFavorite = isFavorite(data.coord.lat, data.coord.lon);
+
+    const handleToggleFavorite = () => {
+        if(isCurrentlyFavorite){
+            removeFavorite.mutate(`${data.coord.lat}-${data.coord.lat}`);
+            toast.error(`Removed ${data.name} from Favorites`);
+        } else {
+            addFavorite.mutate ({
+                name : data.name,
+                lat: data.coord.lat,
+                lon: data.coord.lon,
+                country: data.sys.country,
+            });
+            toast.success(`Added ${data.name} to Favorites`)
+        }
+    };
+
+  return (
+    <Button 
+        variant={isCurrentlyFavorite ? "default" : "outline"}
+        size={"icon"}
+        onClick={handleToggleFavorite}
+        className={isCurrentlyFavorite ? "bg-yellow-500 hover:bg-amber-600" : ""}
+    >
+        <Star className={`h-4 w-4 ${isCurrentlyFavorite ? "fill-current" : ""}`} />
+    </Button>
+  )
+}
+
+export default FavoriteButton 
